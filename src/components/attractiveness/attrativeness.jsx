d7f0c19e35389ecaa814d4c9bcd383db5a38cc75
SergeyBogomolovv <142051687+SergeyBogomolovv@users.noreply.github.com>
2023-11-07 22:06:16 +0300

    add

--- src/components/attractiveness/attrativeness.jsx
+++ src/components/attractiveness/attrativeness.jsx
@@ -4,9 +4,9 @@ import AttrativeCard from './attractiveCard'
 
 export default function Attrativeness() {
   const [attractive, setAttractive] = useState([
-    {src: './pngs/photo_2023-10-20_17-43-20.jpg', text:'Я хз пока что написать'},
-    {src: './pngs/photo_2023-10-20_17-43-20.jpg', text:'Я хз пока что написат'},
-    {src: './pngs/photo_2023-10-20_17-43-20.jpg', text:'Я хз пока что dsfdsf'}
+    {src: './pngs/naymov.png', text:'Я уже давно не мальчик... А почему анон? Найти меня можно всегда в 405... С уважением, завлаб 405...'},
+    {src: './pngs/dyrka2.png', text:'Легендарная дырка на втором этаже у 28 кабинета, там можно складировать мусорные отходы'},
+    {src: './pngs/photo_2023-10-20_17-43-20.jpg', text:'Обвал потолочных секций в первую неделю, вызвало большой резонанс в соц сетях'}
   ])
 
   return (
